d065a9c54a0dcf17e9d3574c7b1e780de8dd04bc
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 15:36:34 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,13 +5,7 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
+
 
 def select_winner(passengers)
   winner = ""
